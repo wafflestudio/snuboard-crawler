3fc9b217f286e51b03c09ff13de424c651866443
Myunghoon Park <58476210+eldpswp99@users.noreply.github.com>
2021-03-05 17:58:18 +0900

    [Crawler] 크롤링 클래스 모듈화 & 에자공, 기계공 크롤링 추가 (#15)
    * crawler make class add ere me logic
    
    * add newline

---
+++ src/routes/me.ts
@@ -0,0 +1,15 @@
+import {CategoryCrawler} from "../classes/categoryCrawler";
+
+export const me = new CategoryCrawler({
+    departmentName: '기계공학부',
+    departmentCode: 'me', // this value must be equal to the filename
+    baseUrl: 'https://me.snu.ac.kr/ko/board/',
+    categoryTags: {
+        notice: '공지사항',
+        news: '뉴스 및 이벤트',
+        freeboard: '자유게시판',
+        seminar: '세미나',
+        jobs: '취업정보'
+    },
+    excludedTag: '미분류'
+});

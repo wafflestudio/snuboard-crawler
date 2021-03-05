3db13bf8245c202167c652f9f532645223b60a9d
sangmin <min112402@gmail.com>
2021-03-05 15:09:18 +0900

    ie crawler

--- src/routes/routeList.ts
+++ src/routes/routeList.ts
@@ -2,9 +2,11 @@ import { Connection } from 'typeorm';
 import * as cse from './cse';
 import * as ee from './ee';
 import * as cbe from './cbe';
+import * as ie from './ie';
 
 export const routeList: ((connection: Connection) => Promise<void>)[] = [
-    cse.startCrawl, 
-    ee.startCrawl, 
-    cbe.startCrawl
+    cse.startCrawl,
+    ee.startCrawl,
+    cbe.startCrawl,
+    ie.startCrawl,
 ];

48e634fecf8f187d80719dc8efa7498483c7a234
sangmin <min112402@gmail.com>
2021-03-11 22:04:54 +0900

    remove 'page' parameter

--- src/routes/math.ts
+++ src/routes/math.ts
@@ -1,12 +1,12 @@
-import {CategoryCrawler} from "../classes/categoryCrawler";
-import {SCIENCE} from "../constants";
-import {CheerioHandlePageInputs} from "apify/types/crawlers/cheerio_crawler";
-import {SiteData} from "../types/custom-types";
-import {absoluteLink, getOrCreate, getOrCreateTags, saveNotice} from "../utils";
-import {File, Notice} from "../../server/src/notice/notice.entity";
-import {load} from "cheerio";
-import {strptime} from "../micro-strptime";
-import {RequestQueue} from "apify";
+import { CheerioHandlePageInputs } from 'apify/types/crawlers/cheerio_crawler';
+import { load } from 'cheerio';
+import { RequestQueue } from 'apify';
+import { CategoryCrawler } from '../classes/categoryCrawler';
+import { SCIENCE } from '../constants';
+import { SiteData } from '../types/custom-types';
+import { absoluteLink, getOrCreate, getOrCreateTags, saveNotice } from '../utils';
+import { File, Notice } from '../../server/src/notice/notice.entity';
+import { strptime } from '../micro-strptime';
 
 class MathCrawler extends CategoryCrawler {
     handlePage = async (context: CheerioHandlePageInputs): Promise<void> => {
@@ -27,17 +27,17 @@ class MathCrawler extends CategoryCrawler {
             notice.title = $('div.titleAndUser div.title h2 a, div.titleAndUser div.title h1 a ').text().trim();
             const contentElement = $('div.contentBody div.xe_content');
             let content;
-            if($('div.contentBody div.xe_content div.document_popup_menu').length){
+            if ($('div.contentBody div.xe_content div.document_popup_menu').length) {
                 contentElement.find('div.document_popup_menu').remove();
                 content = contentElement.html() ?? '';
-            }else {
+            } else {
                 content = contentElement.html() ?? '';
             }
             content = load(content, { decodeEntities: false })('body').html() ?? '';
             // ^ encode non-unicode letters with utf-8 instead of HTML encoding
             notice.content = content;
             notice.preview = contentElement.text().substring(0, 1000).trim(); // texts are automatically utf-8 encoded
-            const fullDateString: string = $('div.dateAndCount div.date').text().trim()
+            const fullDateString: string = $('div.dateAndCount div.date').text().trim();
             // example: '2021-02-26 11:34:01'
             notice.createdAt = strptime(fullDateString, '%Y.%m.%d %H:%M:%S');
 
@@ -65,7 +65,7 @@ class MathCrawler extends CategoryCrawler {
                 }),
             );
 
-            let tags: string[] = [];
+            const tags: string[] = [];
             const urlInstance = new URL(request.loadedUrl);
             const category = urlInstance.searchParams.get('mid');
             if (category && this.categoryTags[category]) {
@@ -76,17 +76,20 @@ class MathCrawler extends CategoryCrawler {
     };
 
     handleList = async (context: CheerioHandlePageInputs, requestQueue: RequestQueue): Promise<void> => {
-        const {request, $} = context;
-        const {url} = request;
+        const { request, $ } = context;
+        const { url } = request;
         const siteData = <SiteData>request.userData;
-        this.log.info('Page opened.', {url});
+        this.log.info('Page opened.', { url });
         if ($) {
             $('table.boardList tbody tr').each((index, element) => {
-                const titleElement = $(element).find('td.title a').first()
+                const titleElement = $(element).find('td.title a').first();
                 // const title = titleElement.text();
 
-                const link = absoluteLink(titleElement.attr('href'), request.loadedUrl);
+                let link = absoluteLink(titleElement.attr('href'), request.loadedUrl);
                 if (link === undefined) return;
+                const pageUrl = new URL(link);
+                pageUrl.searchParams.delete('page');
+                link = pageUrl.href;
 
                 const newSiteData: SiteData = {
                     department: siteData.department,
@@ -94,7 +97,7 @@ class MathCrawler extends CategoryCrawler {
                     isList: false,
                     dateString: '',
                 };
-                this.log.info('Enqueueing', {link});
+                this.log.info('Enqueueing', { link });
                 requestQueue.addRequest({
                     url: link,
                     userData: newSiteData,
@@ -112,10 +115,10 @@ class MathCrawler extends CategoryCrawler {
 
             if (page < endPage) {
                 const nextUrlInstance = new URL(urlInstance.href);
-                nextUrlInstance.searchParams.set('page',(page+1).toString());
+                nextUrlInstance.searchParams.set('page', (page + 1).toString());
                 const nextList = nextUrlInstance.href;
 
-                this.log.info('Enqueueing list', {nextList});
+                this.log.info('Enqueueing list', { nextList });
                 const nextListSiteData: SiteData = {
                     department: siteData.department,
                     isPinned: false,
@@ -138,6 +141,6 @@ export const math = new MathCrawler({
     baseUrl: 'http://www.math.snu.ac.kr/board/index.php?mid=',
     categoryTags: {
         notice: '공지사항',
-        employment: '행사/취업'
+        employment: '행사/취업',
     },
 });

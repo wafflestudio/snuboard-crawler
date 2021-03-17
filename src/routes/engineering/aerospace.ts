931888eb9a5ea96afd15133acbca56cd3eb2dc17
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-03-18 03:01:42 +0900

    Add aerospace crawler

---
+++ src/routes/engineering/aerospace.ts
@@ -0,0 +1,131 @@
+// filename must equal to first level of url domain.
+// e.g. aerospace.snu.ac.kr -> aerospace.ts
+
+import { CheerioHandlePageInputs } from 'apify/types/crawlers/cheerio_crawler';
+import { RequestQueue } from 'apify';
+import { load } from 'cheerio';
+import { Notice, File } from '../../../server/src/notice/notice.entity.js';
+import { SiteData } from '../../types/custom-types';
+import { absoluteLink, getOrCreate, getOrCreateTags, saveNotice } from '../../utils';
+import { strptime } from '../../micro-strptime';
+import { Crawler } from '../../classes/crawler';
+import { ENGINEERING } from '../../constants';
+
+class AerospaceCrawler extends Crawler {
+    handlePage = async (context: CheerioHandlePageInputs): Promise<void> => {
+        const { request, $ } = context;
+        const { url } = request;
+        const siteData = <SiteData>request.userData;
+
+        this.log.info('Page opened.', { url });
+        if ($ !== undefined) {
+            // creation order
+            // dept -> notice -> file
+            //                -> tag -> notice_tag
+
+            const notice = await getOrCreate(Notice, { link: url }, false);
+
+            notice.department = siteData.department;
+            notice.title = $('h1.bbstitle').text().trim();
+            const contentElement = $('div.bbs_contents');
+            notice.content = load(contentElement.html() ?? '', { decodeEntities: false })('body').html() ?? '';
+            // ^ encode non-unicode letters with utf-8 instead of HTML encoding
+            notice.preview = contentElement.text().substring(0, 1000).trim(); // texts are automatically utf-8 encoded
+            notice.createdAt = strptime(siteData.dateString, '%Y-%m-%d');
+
+            notice.isPinned = siteData.isPinned;
+            notice.link = url;
+
+            await saveNotice(notice);
+
+            const files: File[] = [];
+            $('div.att-file ul li a').each((index, element) => {
+                const fileUrl = absoluteLink($(element).attr('href'), url);
+                if (fileUrl !== undefined) {
+                    const file = new File();
+                    file.name = $(element).text().trim();
+                    file.link = fileUrl;
+                    files.push(file);
+                }
+            });
+            await Promise.all(
+                // using Promise.all in order to ensure full execution
+                files.map(async (file) => {
+                    file.notice = notice;
+                    await getOrCreate(File, file);
+                }),
+            );
+
+            const tags: string[] = [];
+            if (siteData.tag !== undefined) {
+                tags.push(siteData.tag);
+            }
+            await getOrCreateTags(tags, notice, siteData.department);
+        } else {
+            throw new TypeError('Selector is undefined');
+        }
+    };
+
+    handleList = async (context: CheerioHandlePageInputs, requestQueue: RequestQueue): Promise<void> => {
+        const { request, $ } = context;
+        const { url } = request;
+        const siteData = <SiteData>request.userData;
+        this.log.info('Page opened.', { url });
+
+        if ($ !== undefined) {
+            $('tbody tr').each((index, element) => {
+                const titleElement = $(element).find('td.title a');
+
+                const relativeLink = titleElement.attr('href');
+                if (relativeLink === undefined) return;
+                const pageUrl = new URL(relativeLink, url);
+                pageUrl.searchParams.delete('page');
+                const link = pageUrl.href;
+
+                let tag = $(element).find('td:nth-child(2)').text();
+                if (tag === '') tag = '미분류';
+
+                const dateString = $(element).find('td:nth-child(4)').text().trim();
+
+                const isPinned = $(element).hasClass('noti');
+
+                const newSiteData: SiteData = {
+                    department: siteData.department,
+                    isPinned,
+                    isList: false,
+                    dateString,
+                    tag,
+                };
+                this.log.info('Enqueueing', { link });
+                requestQueue.addRequest({
+                    url: link,
+                    userData: newSiteData,
+                });
+            });
+
+            const nextList = absoluteLink($('.pagination-01 ul.pager li.next a').attr('href'), url);
+            if (nextList === url || nextList === undefined) return;
+
+            this.log.info('Enqueueing list', { nextList });
+            const nextListSiteData: SiteData = {
+                department: siteData.department,
+                isPinned: false,
+                isList: true,
+                dateString: '',
+            };
+            await this.addVaryingRequest(requestQueue, {
+                url: nextList,
+                userData: nextListSiteData,
+            });
+        } else {
+            throw new TypeError('Selector is undefined');
+        }
+    };
+}
+
+export const aerospace = new AerospaceCrawler({
+    departmentName: '항공우주공학과',
+    departmentCode: 'aerospace',
+    baseUrl: 'https://aerospace.snu.ac.kr/board/notice',
+    departmentCollege: ENGINEERING,
+});

86ccc84f6a14995d23f9c821e8e903ec75e3d3f4
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-04-18 23:33:42 +0900

    Update categoryCrawler.ts

--- src/classes/categoryCrawler.ts
+++ src/classes/categoryCrawler.ts
@@ -220,6 +220,6 @@ export class CategoryCrawler extends Crawler {
             );
         }
         await this.runCrawler(requestQueue, this.handlePage, this.handleList, crawlerOption);
-        this.log.info('Crawl Ended');
+        this.log.info('Crawler Ended');
     };
 }

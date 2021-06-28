ccb968a3954f2ba75f5b3b899dba6ed4073a8a03
Sangmin Lee <min112402@gmail.com>
2021-06-28 21:52:09 +0900

    Update math crawler

--- src/routes/science/math.ts
+++ src/routes/science/math.ts
@@ -1,14 +1,14 @@
 import { CheerioHandlePageInputs } from 'apify/types/crawlers/cheerio_crawler';
 import { load } from 'cheerio';
 import { RequestQueue } from 'apify';
-import { CategoryCrawler } from '../../classes/categoryCrawler';
 import { SCIENCE } from '../../constants';
 import { SiteData } from '../../types/custom-types';
-import { absoluteLink, getOrCreate, getOrCreateTags, saveNotice } from '../../utils';
+import { absoluteLink, getOrCreate, getOrCreateTags, removeUrlPageParam, saveNotice } from '../../utils';
 import { File, Notice } from '../../../server/src/notice/notice.entity';
 import { strptime } from '../../micro-strptime';
+import { Crawler } from '../../classes/crawler';
 
-class MathCrawler extends CategoryCrawler {
+class MathCrawler extends Crawler {
     handlePage = async (context: CheerioHandlePageInputs): Promise<void> => {
         const { request, $ } = context;
         const { url } = request;
@@ -24,22 +24,14 @@ class MathCrawler extends CategoryCrawler {
             const notice = await getOrCreate(Notice, { link: url }, false);
 
             notice.department = siteData.department;
-            notice.title = $('div.titleAndUser div.title h2 a, div.titleAndUser div.title h1 a ').text().trim();
-            const contentElement = $('div.contentBody div.xe_content');
-            let content;
-            if ($('div.contentBody div.xe_content div.document_popup_menu').length) {
-                contentElement.find('div.document_popup_menu').remove();
-                content = contentElement.html() ?? '';
-            } else {
-                content = contentElement.html() ?? '';
-            }
-            content = load(content, { decodeEntities: false })('body').html() ?? '';
+            notice.title = $('div.top_area h1 a').text().trim();
+            const contentElement = $('div.rd_body div.xe_content');
+            const content = load(contentElement.html() ?? '', { decodeEntities: false })('body').html() ?? '';
             // ^ encode non-unicode letters with utf-8 instead of HTML encoding
             notice.content = content;
-            notice.contentText = contentElement.text().trim(); // texts are automatically utf-8 encoded
-            const fullDateString: string = $('div.dateAndCount div.date').text().trim();
-            // example: '2021-02-26 11:34:01'
-            notice.createdAt = strptime(fullDateString, '%Y.%m.%d %H:%M:%S');
+            notice.contentText = contentElement.text().trim();
+            const dateString: string = $('div.fr span.date').text().trim();
+            notice.createdAt = strptime(dateString, '%Y.%m.%d %H:%M');
 
             notice.isPinned = siteData.isPinned;
             notice.link = url;
@@ -47,7 +39,7 @@ class MathCrawler extends CategoryCrawler {
             await saveNotice(notice);
 
             const files: File[] = [];
-            $('div.fileAttached ul li a').each((index, element) => {
+            $('div.rd_ft table li a').each((index, element) => {
                 const fileUrl = $(element).attr('href');
                 if (fileUrl) {
                     const file = new File();
@@ -66,10 +58,11 @@ class MathCrawler extends CategoryCrawler {
             );
 
             const tags: string[] = [];
-            const urlInstance = new URL(request.loadedUrl);
-            const category = urlInstance.searchParams.get('mid');
-            if (category && this.categoryTags[category]) {
-                tags.push(this.categoryTags[category]);
+            const tag = $('div.top_area strong.cate').text().trim();
+            if (tag) {
+                tags.push(tag);
+            } else {
+                tags.push('미분류');
             }
             await getOrCreateTags(tags, notice, siteData.department);
         } else {
@@ -83,15 +76,13 @@ class MathCrawler extends CategoryCrawler {
         const siteData = <SiteData>request.userData;
         this.log.info('Page opened.', { url });
         if ($ !== undefined) {
-            $('table.boardList tbody tr').each((index, element) => {
-                const titleElement = $(element).find('td.title a').first();
+            $('table tbody tr').each((index, element) => {
+                const titleElement = $(element).find('td.title a').last();
                 // const title = titleElement.text();
 
                 let link = absoluteLink(titleElement.attr('href'), request.loadedUrl);
+                link = removeUrlPageParam(link);
                 if (link === undefined) return;
-                const pageUrl = new URL(link);
-                pageUrl.searchParams.delete('page');
-                link = pageUrl.href;
 
                 const newSiteData: SiteData = {
                     department: siteData.department,
@@ -106,15 +97,11 @@ class MathCrawler extends CategoryCrawler {
                 });
             });
 
-            const endElement = $('div.pagination.a1 a.nextEnd');
-            const endUrl = absoluteLink(endElement.attr('href'), request.loadedUrl);
-            const endUrlInstance = new URL(endUrl ?? '');
             const urlInstance = new URL(request.loadedUrl);
-
-            const endPage = +(endUrlInstance.searchParams.get('page') ?? 1);
             const page = +(urlInstance.searchParams.get('page') ?? 1);
+            const lastNoticeId = +$('table tbody tr').last().find('td').first().text().trim();
 
-            if (page < endPage) {
+            if (lastNoticeId > 1) {
                 const nextUrlInstance = new URL(urlInstance.href);
                 nextUrlInstance.searchParams.set('page', (page + 1).toString());
                 const nextList = nextUrlInstance.href;
@@ -146,9 +133,5 @@ export const math = new MathCrawler({
     departmentName: '수리과학부',
     departmentCode: 'math', // this value must be equal to the filename
     departmentCollege: SCIENCE,
-    baseUrl: 'http://www.math.snu.ac.kr/board/index.php?mid=',
-    categoryTags: {
-        notice: '공지사항',
-        employment: '행사/취업',
-    },
+    baseUrl: 'http://www.math.snu.ac.kr/board/index.php?mid=notice',
 });

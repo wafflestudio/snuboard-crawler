f0ca3a1d1fb76c4c439e3e9df92ffd9c5fcdf58b
Sangmin Lee <min112402@gmail.com>
2021-07-25 13:07:45 +0900

    Fix russian tag

--- src/routes/humanities/russian.ts
+++ src/routes/humanities/russian.ts
@@ -12,4 +12,5 @@ export const russian = new GeogCrawler({
         'board-31-GN-1306Xk6x-20210222203800': '채용/행사',
         'board-31-GN-gm2v0p43-20210323172210': '코로나19',
     },
+    excludedTags: ['3', '6'],
 });

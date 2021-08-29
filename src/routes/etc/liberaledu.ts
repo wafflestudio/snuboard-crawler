5f4be77d059c1aeb1131bb1d735e40495d483718
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-08-30 03:29:17 +0900

    Fix Crawler

--- src/routes/etc/liberaledu.ts
+++ src/routes/etc/liberaledu.ts
@@ -1,134 +1,16 @@
 // filename must equal to first level of url domain.
 // e.g. liberaledu.snu.ac.kr -> liberaledu.ts
 
-import { CheerioHandlePageInputs } from 'apify/types/crawlers/cheerio_crawler';
-import { RequestQueue } from 'apify';
-import { load } from 'cheerio';
-import { URL } from 'url';
-import { Notice, File } from '../../../server/src/notice/notice.entity.js';
-import { CategoryTag, SiteData } from '../../types/custom-types';
-import { absoluteLink, getOrCreate, getOrCreateTagsWithMessage, saveNotice } from '../../utils';
-import { strptime } from '../../micro-strptime';
-import { Crawler } from '../../classes/crawler';
 import { ETC } from '../../constants';
+import { CategoryCrawler } from '../../classes/categoryCrawler';
 
-class LiberaleduCrawler extends Crawler {
-    handlePage = async (context: CheerioHandlePageInputs): Promise<void> => {
-        const { request, $ } = context;
-        const { url } = request;
-        const siteData = <SiteData>request.userData;
-
-        this.log.info('Page opened.', { url });
-        if ($ !== undefined) {
-            const notice = await getOrCreate(Notice, { link: url }, false);
-
-            notice.department = siteData.department;
-            const title = $('#bltnTitle').text().trim();
-            notice.title = title;
-            const contentElement = $('#print > table.table_type01.view > tbody > tr:nth-child(1) > td');
-            const content = load(contentElement.html() ?? '', { decodeEntities: false })('body').html() ?? '';
-            notice.content = content;
-            notice.contentText = contentElement.text().trim(); // texts are automatically utf-8 encoded
-            notice.createdAt = strptime(siteData.dateString, '%Y.%m.%d');
-
-            notice.isPinned = siteData.isPinned;
-            notice.link = url;
-
-            await saveNotice(notice);
-
-            const files: File[] = [];
-            $('ul.file_list li a').each((index, element) => {
-                const fileUrl = $(element).attr('href');
-                if (fileUrl) {
-                    const file = new File();
-                    file.name = $(element).text().trim();
-                    file.link = absoluteLink(fileUrl, this.baseUrl) ?? notice.link;
-                    files.push(file);
-                }
-            });
-            await Promise.all(
-                // using Promise.all in order to ensure full execution
-                files.map(async (file) => {
-                    file.notice = notice;
-                    await getOrCreate(File, file);
-                }),
-            );
-
-            const tags: string[] = ['공지사항'];
-            await getOrCreateTagsWithMessage(tags, notice, siteData.department);
-        } else {
-            throw new TypeError('Selector is undefined');
-        }
-    };
-
-    handleList = async (context: CheerioHandlePageInputs, requestQueue: RequestQueue): Promise<void> => {
-        const { request, $ } = context;
-        const { url } = request;
-        const siteData = <SiteData>request.userData;
-        this.log.info('Page opened.', { url });
-
-        if ($ !== undefined) {
-            $('tbody tr').each((index, element) => {
-                const titleElement = $(element).find('td.subject div a');
-                const s = titleElement.attr('href');
-                if (s === undefined) return;
-                const apiBoardNum = s.trim().match(/Bulletin\('([0-9]+)','([0-9]+)'\)/)?.[1];
-                const apiNoticeNum = s.trim().match(/Bulletin\('([0-9]+)','([0-9]+)'\)/)?.[2];
-                const link = `https://board.snu.ac.kr/apiboard/${apiBoardNum}/${apiNoticeNum}`;
-                const dateString = $(element).find('td:nth-child(4)').text().trim();
-                if (!dateString) return;
-                const noticeNum = $(element).children('td').first().text().trim();
-                const isPinned = noticeNum === '공지';
-                const newSiteData: SiteData = {
-                    department: siteData.department,
-                    isPinned,
-                    isList: false,
-                    dateString,
-                    commonUrl: siteData.commonUrl,
-                    tag: siteData.tag,
-                };
-                this.log.info('Enqueueing', { link });
-                requestQueue.addRequest({
-                    url: link,
-                    userData: newSiteData,
-                });
-            });
-
-            const urlInstance = new URL(url);
-            const page: number = +(urlInstance.searchParams.get('page') ?? 1);
-            const lastNoticeId = $('table tbody tr').last().children('td').first().text().trim();
-            if (!lastNoticeId || Number.isNaN(+lastNoticeId)) return;
-            if (+lastNoticeId > 1) {
-                const nextListUrlInstance = new URL(url);
-                nextListUrlInstance.searchParams.set('page', `${page + 1}`);
-                const nextList: string = nextListUrlInstance.href;
-                this.log.info('Enqueueing list', { nextList });
-                const nextListSiteData: SiteData = {
-                    department: siteData.department,
-                    isPinned: false,
-                    isList: true,
-                    dateString: '',
-                    commonUrl: siteData.commonUrl,
-                    tag: siteData.tag,
-                };
-                await this.addVaryingRequest(
-                    requestQueue,
-                    {
-                        url: nextList,
-                        userData: nextListSiteData,
-                    },
-                    nextListSiteData.commonUrl,
-                );
-            }
-        } else {
-            throw new TypeError('Selector is undefined');
-        }
-    };
-}
-
-export const liberaledu = new LiberaleduCrawler({
+export const liberaledu = new CategoryCrawler({
     departmentName: '기초교육원',
     departmentCode: 'liberaledu',
-    baseUrl: 'https://liberaledu.snu.ac.kr/board/notice',
     departmentCollege: ETC,
+    baseUrl: 'https://liberaledu.snu.ac.kr/board/',
+    categoryTags: {
+        notice: '공지사항',
+    },
+    excludedTag: '미분류',
 });

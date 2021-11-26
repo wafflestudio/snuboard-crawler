51e82f4e52e56ef04e45d1973a092623ff736d56
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-11-26 23:43:22 +0900

    Add typing

--- src/routes/routeList.ts
+++ src/routes/routeList.ts
@@ -163,7 +163,7 @@ export const earlyStopList: Crawler[] = [
     design,
 ];
 
-const populationList = [];
+const populationList: Crawler[] = [];
 
 const POPULATION = TRUE_STRING.includes(process.env.POPULATION ?? '');
 const crawlerList = POPULATION ? populationList : earlyStopList;

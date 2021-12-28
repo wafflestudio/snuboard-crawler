f29e2b58f38d762a4ab34773ff73e9000c09cf39
eldpswp99 <nrg1392@naver.com>
2021-12-28 20:12:18 +0900

    fcm android & iOS separate notification

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,10 +4,24 @@ import { fbConfig } from './fbconfig';
 import { Department } from '../server/src/department/department.entity';
 import { Notice } from '../server/src/notice/notice.entity';
 import { parseTagsToStringWithSeparator } from './utils';
-import { messaging } from 'firebase-admin';
-import MessagingOptions = messaging.MessagingOptions;
+// import MessagingOptions = messaging.MessagingOptions;
+// import TokenMessage = messaging.TokenMessage;
 
 admin.initializeApp({ credential: admin.credential.cert(fbConfig) });
+
+const getPayload = (notice: Notice, department: Department, tags: string[]) => {
+    const previewLength = 250;
+
+    return {
+        title: `${department.name} 신규 공지사항입니다`,
+        body: notice.title,
+        noticeId: `${notice.id}`,
+        departmentName: department.name,
+        departmentId: `${department.id}`,
+        preview: notice.contentText.slice(0, previewLength),
+        tags: parseTagsToStringWithSeparator(tags, ';'),
+    };
+};
 
 export async function sendNoticeCreationMessage(
     condition: string,
@@ -15,22 +29,20 @@ export async function sendNoticeCreationMessage(
     department: Department,
     tags: string[],
 ) {
-    const previewLength = 250;
-
-    const payload = {
-        data: {
-            title: `${department.name} 신규 공지사항입니다`,
-            body: notice.title,
-            noticeId: `${notice.id}`,
-            departmentName: department.name,
-            departmentId: `${department.id}`,
-            preview: notice.contentText.slice(0, previewLength),
-            tags: parseTagsToStringWithSeparator(tags, ';'),
+    const testMessage = {
+        android: {
+            data: getPayload(notice, department, tags),
         },
+        apns: {
+            payload: {
+                aps: {
+                    alert: getPayload(notice, department, tags),
+                    contentAvailable: true,
+                },
+                customKey: 'customValue',
+            },
+        },
+        condition,
     };
-    const options: MessagingOptions = {
-        contentAvailable: true,
-    };
-
-    await admin.messaging().sendToCondition(condition, payload, options);
+    await admin.messaging().send(testMessage);
 }
